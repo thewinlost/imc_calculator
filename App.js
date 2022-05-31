debb1d33169505018fb8a75ca84faa1859d63065
thewinlost <vitoriajardim.bg151@academico.ifsul.edu.br>
2022-05-31 11:07:14 -0300

    master

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*jshint esversion:8*/
+import React, { Component } from "react";
+import { Text, View, TouchableOpacity, TextInput } from "react-native";
+import MeuEstilo from "./src/meuestilo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Calculadora extends React.Component {
+  state = {
+    peso: 0.0,
+    altura: 0.0,
+    resultado: 0.0,
+  };
+  atualizaPeso = (number) => {
+    this.setState({ peso: number });
+  };
+  atualizaAltura = (number) => {
+    this.setState({ altura: number });
+  };
+  calculo() {
+    this.state.resultado =
+      parseFloat(this.state.peso) /
+      (parseFloat(this.state.altura) * parseFloat(this.state.altura));
+    if (this.state.resultado <= 18) {
+      alert("seu imc é:" + this.state.resultado + "ta se alimentando?");
+    } else if (this.state.resultado >= 30) {
+      alert("oi gordinho, seu imc é:" + this.state.resultado);
+    } else {
+      alert(
+        "seu imc é:" +
+          this.state.resultado +
+          "estás nem gordo, nem magro, just basic"
+      );
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          style={MeuEstilo.input}
+          underlineColorAndroid="transparent"
+          placeholder="Digite o peso"
+          placeholderTextColor="black"
+          autoCapitalize="none"
+          onChangeText={this.atualizaPeso}
+          keyboardType="numeric"
+        />
+
+        <TextInput
+          style={MeuEstilo.input}
+          underlineColorAndroid="transparent"
+          placeholder="Digite a altura"
+          placeholderTextColor="black"
+          autoCapitalize="none"
+          onChangeText={this.atualizaAltura}
+          keyboardType="numeric"
+        />
+
+        <TouchableOpacity
+          onPress={() =>
+            this.calculo(
+              this.state.peso,
+              this.state.altura,
+              this.state.resultado
+            )
+          }
+        >
+          <Text style={MeuEstilo.submitButtonText}> continhas </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/meuestilo.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+export default StyleSheet.create({
+  input: {
+    margin: 25,
+    height: 40,
+    width: 600,
+    borderColor: "#FF69B4",
+    borderWidth: 1,
+  },
+
+  submitButtonText: {
+    color: "white",
+    backgroundColor: "#FF69B4",
+    padding: 10,
+    height: 40,
+    width: 80,
+    margin: 20,
+    fontSize: 14,
+  },
+});
